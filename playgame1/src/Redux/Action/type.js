f8f9bd9907177682af9acd7aa58c7f9c99f65691
phuongthao0506 <85739218+phuongthao0506@users.noreply.github.com>
2021-07-17 23:47:16 +0700

    t

--- playgame1/src/Redux/Action/type.js
+++
@@ -1,5 +0,0 @@
-export const PLAY_GAME = 'PLAY_GAME';
-export const DAT_CUOC = 'DAT_CUOC';
-export const DAT_CUOC_GAME = 'DAT_CUOC_GAME'
-export const RANDOM_GAME = 'RANDOM_GAME'
-export const END_GAME = 'END_GAME'
