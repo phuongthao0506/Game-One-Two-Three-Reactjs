84fb68d9264522ffab6d878b60bc2983a5e7559e
phuongthao0506 <85739218+phuongthao0506@users.noreply.github.com>
2021-07-17 23:15:17 +0700

    Your awesome message

---
+++ playgame1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import GameIndex from './Redux/playGame/GameOneTwoThree/GameIndex';
+// import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+   <GameIndex/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ playgame1/src/Redux/Reducer/rootReducer.jsx
@@ -0,0 +1,6 @@
+//import redux
+import {combineReducers} from 'redux'
+import GameReducer from './GameReducer.jsx'
+export const rootReducer = combineReducers({
+    GameReducer
+})
